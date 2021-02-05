fc0112b4af53c006c408cbd11421410af91f6423
Ensar Uç <69520181+ensaruc@users.noreply.github.com>
2021-02-05 14:33:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] f1Takimlari = new string[] { "Mercedes", "Red Bull", "Ferrari" };
+
+            for (int i = 0; i < f1Takimlari.Length; i++)
+            {
+                Console.WriteLine(f1Takimlari[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string f1Takim in f1Takimlari)
+            {
+                Console.WriteLine(f1Takim);
+            }
+
+
+            for (int i = 1; i < 10; i=i+1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
